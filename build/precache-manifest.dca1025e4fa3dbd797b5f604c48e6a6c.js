f7d633c567ac50c7a8c763571fe272b42003fa9a
kanishkaisuru <sampathisuru516@gmail.com>
2020-04-20 00:40:22 +0530

    test

---
+++ build/precache-manifest.dca1025e4fa3dbd797b5f604c48e6a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ae780873e7348ee0663",
+    "url": "/static/css/main.a24f262a.chunk.css"
+  },
+  {
+    "revision": "9ae780873e7348ee0663",
+    "url": "/static/js/main.2fe281b1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bb41cce1897c33a8ec13a1dbf38767bc",
+    "url": "/index.html"
+  }
+];
